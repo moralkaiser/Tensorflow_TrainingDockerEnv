48c7d89d3bb15307a38ae068b35978e410e632b5
admin <admin@mail.com>
2020-02-10 01:58:41 +0100

    Fixed Minor Issue with Path in bootDocker.sh

--- AdminScripts/startContainerPhpApi.sh
+++ AdminScripts/startContainerPhpApi.sh
@@ -1,7 +1,8 @@
 
 if [ ! -z $1 ]; then
-        docker exec -d -w /tensorflow/models/research/volume/scripts/bash tf_trainingcontainer_$1 bash ./startPhpApi.sh 800
-	docker exec -d -w /tensorflow/models/research/volume/scripts/bash tf_trainingcontainer_$1 bash ./startPhpApi.sh 801
+echo $1
+ docker exec -d -w /tensorflow/models/research/volume/scripts/bash tf_trainingcontainer_$1 bash ./startPhpApi.sh 800	
+ docker exec -d -w /tensorflow/models/research/volume/scripts/bash tf_trainingcontainer_$1 bash ./startPhpApi.sh 801
 else
         RED='\033[0;31m'
         NC='\033[0m' # No Color
--- Resources/BashScripts/bootDocker.sh
+++ Resources/BashScripts/bootDocker.sh
@@ -13,6 +13,6 @@ IFS=$SAVEIFS   # Restore IFS
 #for i in "${userhandle[@]}"; do echo "$i"; echo next; done
 
 docker kill "tf_trainingcontainer_$userhandle"
-docker run --network=tf_containerlink_${userhandle[0]} --runtime=nvidia -p ${userhandle[1]}:8888 -v /home/teder/testArchiv/Mustererkennung_Job/UserContainerVolumes/${userhandle[0]}:/tensorflow/models/research/volume -itd --rm --name "tf_trainingcontainer_${userhandle[0]}" tf_trainingcontainer_student:latest
+docker run --network=tf_containerlink_${userhandle[0]} --runtime=nvidia -p ${userhandle[1]}:8888 -v $(pwd):/tensorflow/models/research/volume -itd --rm --name "tf_trainingcontainer_${userhandle[0]}" tf_trainingcontainer_student:latest
 #docker exec -it --user=root -e PYTHONPATH=$PYTHONPATH:/tensorflow/models/research:/tensorflow/models/research/slim -w /tensorflow/models/research/volume/scripts/bash "tf_trainingcontainer_$userhandle" bash ./startDatabuildAndTraining.sh
 
