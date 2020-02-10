6886b522833a4c296e02297b442b118e69749498
admin <admin@mail.com>
2020-02-10 01:59:53 +0100

    Minor Fix

--- AdminScripts/startContainerPhpApi.sh
+++ AdminScripts/startContainerPhpApi.sh
@@ -1,6 +1,5 @@
 
 if [ ! -z $1 ]; then
-echo $1
  docker exec -d -w /tensorflow/models/research/volume/scripts/bash tf_trainingcontainer_$1 bash ./startPhpApi.sh 800	
  docker exec -d -w /tensorflow/models/research/volume/scripts/bash tf_trainingcontainer_$1 bash ./startPhpApi.sh 801
 else
