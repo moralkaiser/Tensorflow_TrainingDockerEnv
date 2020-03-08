67313c1835a6d70c845f2807e1d74ddad9ba893b
moralkaiser <59457050+moralkaiser@users.noreply.github.com>
2020-03-08 15:20:50 +0100

    added more Ports to Workload Management

--- AdminScripts/launchWorkloadManagement.sh
+++ AdminScripts/launchWorkloadManagement.sh
@@ -3,4 +3,7 @@ if [ ! "$(docker ps -q -f name=workloadmanagementContainer)" ]; then
 	docker run -v $(pwd):/volume --network=tf_containerlink -itd --rm --name=workloadmanagementContainer workloadmanagement:latest
 	docker exec -d -w /volume workloadmanagementContainer bash ./startPhpApi.sh 800
 	docker exec -d -w /volume workloadmanagementContainer bash ./startPhpApi.sh 801
+	docker exec -d -w /volume workloadmanagementContainer bash ./startPhpApi.sh 803
+	docker exec -d -w /volume workloadmanagementContainer bash ./startPhpApi.sh 804
+	docker exec -d -w /volume workloadmanagementContainer bash ./startPhpApi.sh 805
 fi
